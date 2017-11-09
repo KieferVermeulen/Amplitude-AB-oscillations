ba8e77b692bdc52a70c7bf0bbd0cf0352cc42ee7
KieferVermeulen <33250887+KieferVermeulen@users.noreply.github.com>
2017-11-09 15:20:45 +0100

    Fixed order

--- Amplitude_Determination.py
+++ Amplitude_Determination.py
@@ -1,6 +1,11 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import numpy as np
 
+Data = np.loadtxt('L2_L10_magnet_Z_large_sweep_topgate_18_T_412mK_1.dat')
+Time = Data[:,0]
+Time = np.abs(Time + 9.990000000000e-02)
+Amp = Data[:,5]
 noise = np.random.normal(0,0.001,10000)
 
 
@@ -12,12 +17,3 @@ fk=np.fft.fft(noise/n)
 plt.plot(nu,fk.real,'r')
 plt.show()
 print(dt)
-
-
-import numpy as np
-
-Data = np.loadtxt('L2_L10_magnet_Z_large_sweep_topgate_18_T_412mK_1.dat')
-Time = Data[:,0]
-Time = np.abs(Time + 9.990000000000e-02)
-Amp = Data[:,5]
-
