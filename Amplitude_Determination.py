2c39841319ff57d89fee3d16ad7aef3294a7e91b
KieferVermeulen <33250887+KieferVermeulen@users.noreply.github.com>
2017-11-10 16:02:45 +0100

    Added smoothing of function. Also made it such that 0 frequency is no longer dominant

--- Amplitude_Determination.py
+++ Amplitude_Determination.py
@@ -4,15 +4,17 @@ import numpy as np
 import pylab as plb
 from scipy.optimize import curve_fit
 from scipy import asarray as ar,exp
+import scipy.signal
 Data = np.loadtxt('L2_L10_magnet_Z_large_sweep_topgate_18_T_412mK_1.dat')
 Time = Data[:,0]*10000
 Time = np.abs(Time + 9.990000000000e-02)
 Amp = Data[:,5]*12000
 print(Amp)
+
+n=len(Time)
+Amp = scipy.signal.savgol_filter(Amp,125, 2, deriv=0, delta=1.0, axis=-1, mode='interp', cval=0.0)-Amp
 plt.plot(Time,Amp)
 plt.show()
-n=len(Time)
-
 dt=1/150
 nu=np.fft.fftfreq(n,dt)
 fk=np.fft.fft(Amp)/n
@@ -21,18 +23,18 @@ plt.plot(nu,fk.real,'r')
 plt.plot(nu,fk.imag,'r')
 plt.show()
 n = len(nu)                          #the number of data
-mean = sum(nu*fk)/n                   #note this correction
-sigma = sum(fk*(nu-mean)**2)/n        #note this correction
-
-def gaus(x,a,x0,sigma):
-    return a*exp(-(nu-x0)**2/(2*sigma**2))
-
-popt,pcov = curve_fit(gaus,nu,fk,p0=[1,mean,sigma])
-
-plt.plot(nu,fk,'b+:',label='data')
-plt.plot(nu,gaus(nu,*popt),'ro:',label='fit')
-plt.legend()
-plt.title('Fig. 3 - Fit for Time Constant')
-plt.xlabel('Time (s)')
-plt.ylabel('Voltage (V)')
-plt.show()
+# mean = sum(nu*fk)/n                   #note this correction
+# sigma = sum(fk*(nu-mean)**2)/n        #note this correction
+#
+# def gaus(x,a,x0,sigma):
+#     return a*exp(-(nu-x0)**2/(2*sigma**2))
+#
+# popt,pcov = curve_fit(gaus,nu,fk,p0=[1,mean,sigma])
+#
+# plt.plot(nu,fk,'b+:',label='data')
+# plt.plot(nu,gaus(nu,*popt),'ro:',label='fit')
+# plt.legend()
+# plt.title('Fig. 3 - Fit for Time Constant')
+# plt.xlabel('Time (s)')
+# plt.ylabel('Voltage (V)')
+# plt.show()
