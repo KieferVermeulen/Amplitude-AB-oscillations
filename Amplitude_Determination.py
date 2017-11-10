9c79e06b7c516184d8c1dfd766bd9fbaa6a1b132
KieferVermeulen <33250887+KieferVermeulen@users.noreply.github.com>
2017-11-10 16:33:07 +0100

    Did zero padding to smooth out fourier transform. More frequencies over which we can plot

--- Amplitude_Determination.py
+++ Amplitude_Determination.py
@@ -6,14 +6,17 @@ from scipy.optimize import curve_fit
 from scipy import asarray as ar,exp
 import scipy.signal
 Data = np.loadtxt('L2_L10_magnet_Z_large_sweep_topgate_18_T_412mK_1.dat')
-Time = Data[:,0]*10000
+Time = Data[:,0]
 Time = np.abs(Time + 9.990000000000e-02)
 Amp = Data[:,5]*12000
 print(Amp)
 
 n=len(Time)
 Amp = scipy.signal.savgol_filter(Amp,125, 2, deriv=0, delta=1.0, axis=-1, mode='interp', cval=0.0)-Amp
+Amp = np.pad(Amp, 5000, 'constant')
+Time = np.pad(Time,5000,'linear_ramp')
 plt.plot(Time,Amp)
+n=len(Time)
 plt.show()
 dt=1/150
 nu=np.fft.fftfreq(n,dt)
